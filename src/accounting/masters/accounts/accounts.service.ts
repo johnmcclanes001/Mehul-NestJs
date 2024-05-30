c7f30a0227bc124811e3d1a1c214d26351e27e23
Mehul Prajapati <mehul.prajapati@hiddenbrains.in>
2024-05-30 19:03:22 +0530

    Join query

--- src/accounting/masters/accounts/accounts.service.ts
+++ src/accounting/masters/accounts/accounts.service.ts
@@ -9,57 +9,83 @@ import { ChartOfAccountsService } from '../chart_of_accounts/chart_of_accounts.s
 /**
  *  Validation : Account can only be created at the last level of chart of accounts
  *  Generate Code based on chart of account code and max account code + 1
-*/
+ */
 
 @Injectable()
 export class AccountsService {
   constructor(
     @InjectRepository(Account)
-    private readonly repository : Repository<Account>,
-    private readonly coaSevice : ChartOfAccountsService
-  ){}
+    private readonly repository: Repository<Account>,
+    private readonly coaSevice: ChartOfAccountsService,
+  ) {}
 
   async create(createAccountDto: CreateAccountDto) {
     const newCode = await this.generateAccountCode(createAccountDto.coa_id);
-    if(newCode.code === 0){
-      return newCode.msg;  
+    if (newCode.code === 0) {
+      return newCode.msg;
     }
     createAccountDto['code'] = newCode.code;
     return await this.repository.insert(new Account(createAccountDto));
-    return 'This action adds a new account';
   }
 
-  findAll() {
-    return `This action returns all accounts`;
+  async findAll() {
+    return await this.repository
+      .createQueryBuilder('a')
+      .select('a.*,c.name as coa_name, c.category as category, c.type as type')
+      .innerJoin('chart_of_account', 'c', 'a.coa_id = c.id')
+      .getRawMany();
   }
 
   async findOne(id: number) {
-    return await this.coaSevice.findOne(id)
+    return await this.repository
+      .createQueryBuilder('a')
+      .select('a.*,c.name as coa_name, c.category as category, c.type as type')
+      .innerJoin('chart_of_account', 'c', 'a.coa_id = c.id')
+      .where({ id: id })
+      .getRawOne();
     return `This action returns a #${id} account`;
   }
 
-  update(id: number, updateAccountDto: UpdateAccountDto) {
-    return `This action updates a #${id} account`;
+  async update(id: number, updateAccountDto: UpdateAccountDto) {
+    const existingDetails = await this.findOne(id);
+    if (existingDetails) {
+      if (existingDetails.coa_id !== updateAccountDto.coa_id) {
+        const newCode = await this.generateAccountCode(updateAccountDto.coa_id);
+        if (newCode.code === 0) {
+          return newCode.msg;
+        }
+        updateAccountDto['code'] = newCode.code;
+      }
+      await this.repository.update({id:id},new Account(updateAccountDto));
+      return this.findOne(id);
+    }
+    else{
+      return `Record not found.`;
+    }
+    
   }
 
   remove(id: number) {
     return `This action removes a #${id} account`;
   }
 
-  async generateAccountCode(coaId: number){
+  async generateAccountCode(coaId: number) {
     const coaDetails = await this.coaSevice.findOne(coaId);
-    if(coaDetails){
-      let code: string = "";
-      const lastCode = await this.repository.findOne({where : {coa_id:coaId},order:{code:'DESC'}})
-      if(lastCode){
-        code = coaDetails.code + "" + (lastCode.code + 1)
-      }else{
-        code = coaDetails.code + "" + 1;
+    if (coaDetails) {
+      let code: string = '';
+      const lastCode = await this.repository.findOne({
+        where: { coa_id: coaId },
+        order: { code: 'DESC' },
+      });
+      if (lastCode) {
+        code = '' + ++lastCode.code;
+      } else {
+        code = coaDetails.code + '' + 1;
       }
       console.log(code);
-      return {code: parseInt(code), msg:""}
-    }else{
-      return {code: 0, msg:"Coa details not found."}
+      return { code: parseInt(code), msg: '' };
+    } else {
+      return { code: 0, msg: 'Coa details not found.' };
     }
   }
 }
